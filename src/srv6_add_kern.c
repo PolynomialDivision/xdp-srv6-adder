65dff33251abcef89db0878f5b4ea5cfc137fc7b
Polynomialdivision <vincent@systemli.org>
2021-03-28 16:02:12 +0200

    xdp-srv6-adder: add also inline

--- src/srv6_add_kern.c
+++ src/srv6_add_kern.c
@@ -178,4 +178,155 @@ out:
   return XDP_PASS;
 }
 
+
+SEC("srv6-adder-inline")
+int xdp_srv6_add_inline(struct xdp_md *ctx) {
+  volatile struct ethhdr old_ehdr;
+  volatile struct ipv6hdr oldr_ipv6_orig_hdr;
+  void *data_end = (void *)(long)ctx->data_end;
+  void *data = (void *)(long)ctx->data;
+
+  struct ethhdr *ehdr = data;
+  if (ehdr + 1 > data_end) // bounds checking
+    goto out;
+  old_ehdr = *ehdr;
+
+  if (bpf_ntohs(ehdr->h_proto) != ETH_P_IPV6) {
+    goto out;
+  }
+
+  // ----- Copy Orig IPv6 Header -------
+  struct ipv6hdr *ipv6_orig_header = (void *)(ehdr + 1);
+  if (ipv6_orig_header + 1 > data_end)
+    goto out;
+  oldr_ipv6_orig_hdr = *ipv6_orig_header;
+  // ------------------------------------
+
+  // check if link local
+  if (ipv6_orig_header->daddr.s6_addr[0] == 0xfe)
+    goto out;
+
+  // -------- checking --------
+  struct cidr *cidr;
+  int inprefix = 0;
+  int j;
+  for (j = 0; j <= MAX_CIDR; j++) {
+    __u32 key = (__u32)j;
+    cidr = bpf_map_lookup_elem(&prefixmap, &key);
+    if (!cidr)
+      goto loop;
+    int prefix_limit = 15 - ((128 - cidr->prefix) / 8);
+    int i;
+    for (i = 0; i < 16; i++) {
+      __u8 net1 = ipv6_orig_header->saddr.s6_addr[i];
+      __u8 net2 = cidr->addr.v6.s6_addr[i];
+
+      if (i >= prefix_limit)
+        break;
+
+      if (net1 != net2) {
+        goto loop;
+      }
+    }
+    if (i >= 16)
+      goto loop;
+
+    __u8 net1 = ipv6_orig_header->saddr.s6_addr[i];
+    __u8 net2 = cidr->addr.v6.s6_addr[i];
+    __u8 mask = ~((1 << ((128 - cidr->prefix) % 8)) - 1);
+
+    net1 &= mask;
+    net2 &= mask;
+
+    if (net1 != net2) {
+      goto loop;
+    }
+
+    // if we reach here, some prefix is announced
+    inprefix = 1;
+
+    break;
+  loop:
+    continue;
+  }
+
+  if (!inprefix)
+    goto out;
+
+  // ---------------
+  int numsegs = MAX_SEG_LIST + 1;
+  int offset = ipv6_lentest(numsegs);
+  if (bpf_xdp_adjust_head(ctx, -offset)) {
+    goto out;
+  }
+
+  // ------------ Copy Eth-Addr ---------------
+  data_end = (void *)(long)ctx->data_end;
+  ehdr = (void *)(long)ctx->data;
+
+  if (ehdr + 1 > data_end)
+    goto out;
+
+  *ehdr = old_ehdr;
+
+  // ------------------------------------------
+
+  // -------------- Copy Header Back as encap header -----
+  struct ipv6hdr *ip6_srv6_encap = (void *)(ehdr + 1);
+  if (ip6_srv6_encap + 1 > data_end)
+    goto out;
+  *ip6_srv6_encap = oldr_ipv6_orig_hdr;
+  ip6_srv6_encap->nexthdr = IPV6_EXT_ROUTING;
+
+  if(cidr->segleft >= MAX_SEG_LIST)
+    goto out;
+
+  __builtin_memcpy(ip6_srv6_encap->daddr.s6_addr, cidr->segpath[cidr->segleft].s6_addr,
+                   16);
+
+  ip6_srv6_encap->payload_len += bpf_ntohs(offset);
+  ip6_srv6_encap->hop_limit -= bpf_ntohs(MAX_SEG_LIST - cidr->segleft);
+  // ------------------------------------------
+
+  // ------ Create Srv6 Header ------------------
+  struct ip6_addr_t *seg;
+  struct ip6_srh_t *srh;
+
+  srh = (struct ip6_srh_t *)(void *)(ip6_srv6_encap + 1);
+  if (srh + 1 > data_end)
+    goto out;
+
+  srh->nexthdr = oldr_ipv6_orig_hdr.nexthdr;
+  srh->hdrlen = 2 * numsegs;
+  srh->type = 4;
+  srh->segments_left = cidr->segleft + 1;
+  srh->first_segment = MAX_SEG_LIST;
+  srh->flags = 0;
+  srh->tag = 0;
+
+  seg = (struct ip6_addr_t *)(srh + 1);
+
+  if (seg + 1 > data_end)
+    goto out;
+
+  // copy first the actual destination
+  __builtin_memcpy(seg, oldr_ipv6_orig_hdr.daddr.s6_addr, 16);
+
+  // go to next segment
+  seg = (struct ip6_addr_t *)(seg + 1);
+
+  if (seg + MAX_SEG_LIST > data_end)
+    goto out;
+
+#pragma clang loop unroll(full)
+  for (int i = 0; i < MAX_SEG_LIST; i++) {
+    __builtin_memcpy(seg, cidr->segpath[i].s6_addr, 16);
+
+    seg = (struct ip6_addr_t *)(seg + 1);
+  }
+  // ------------------------------------------
+out:
+  return XDP_PASS;
+}
+
 char _license[] SEC("license") = "GPL";
